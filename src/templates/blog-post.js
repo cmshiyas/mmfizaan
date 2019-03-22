d1969765ebfd715b7278842e649f2794ba397791
Shiyas <shiyas.cholamukhath@tabcorp.com.au>
2019-03-23 07:03:00 +1100

    added seo setup

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Layout from '../components/layout';
 import Img from 'gatsby-image';
+import Metatags from '../components/Metatags';
 import { graphql } from 'gatsby';
 function BlogPost(props) {
     const post = props.data.markdownRemark;
